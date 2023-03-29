9df92bef865da3bbc3855ea1eb775a515e6b6787
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-03-29 15:25:29 -0300

    all levels generalized

--- DFA.swiftpm/ContentView.swift
+++ DFA.swiftpm/ContentView.swift
@@ -1,30 +1,92 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var levelNum : Int = 1
+    @State var levelNum : Int = 2
     var body: some View {
+        
         ZStack{
             Color.purple.brightness(-0.8).ignoresSafeArea()
-            Image("background").scaledToFill()
+            Image("background")
+                .resizable()
+                .scaledToFill()
+            
+            // implement ? for doubts
             
             if levelNum == 0 {
-//                onboarding
+                //                onboarding
             }
             
             // take as many 👽 as you need and return to your planet
             if levelNum == 1{
-                Level(levelNum: $levelNum, level: "level\(levelNum)_", bodies: ["👽"], answer: ["👽", "?", "?", "?", "?", "?"], pickerXPos: [0.5], pickerYPos: [0.05], xPos: [0.8, 0.82, 0.5, 0.2, 0.2], yPos: [0.7, 0.15, 0.06, 0.16, 0.68], angles: [110, 50, 0, -50, -120])
+                Level(levelNum: $levelNum,
+                      level: "level\(levelNum)_",
+                      bodies: ["👽"],
+                      answer: ["👽", "?", "?", "?", "?", "?"],
+                      pickerXPos: [0.5],
+                      pickerYPos: [0.05],
+                      xPos: [0.8, 0.82, 0.5, 0.2, 0.2],
+                      yPos: [0.7, 0.15, 0.06, 0.16, 0.68],
+                      angles: [110, 50, 0, -50, -120])
                 
-            // An odd number of 👽
+                // An odd number of 👽
             } else if levelNum == 2{
-                Level(levelNum: $levelNum, level: "level\(levelNum)_", bodies: ["👽", "🌒"], answer: ["👽", "🌒", "👽", "🌒", "?", "?"], pickerXPos: [0.5, 0.93, 0.5, 0.06], pickerYPos: [0.05, 0.5, 0.96, 0.5],  xPos: [0.7, 0.82, 0.5, 0.2, 0.2], yPos: [0.676, 0.15, 0.06, 0.16, 0.68], angles: [160, 50, 0, -50, -120])
+                Level(levelNum: $levelNum,
+                      level: "level\(levelNum)_",
+                      bodies: ["👽", "🌒"],
+                      answer: ["👽", "🌒", "👽", "🌒", "?", "?"],
+                      pickerXPos: [0.5, 0.93, 0.5, 0.06],
+                      pickerYPos: [0.05, 0.5, 0.96, 0.5],
+                      xPos: [0.7, 0.82, 0.5, 0.2, 0.2],
+                      yPos: [0.676, 0.15, 0.06, 0.16, 0.68],
+                      angles: [160, 50, 0, -50, -120])
                 
-            // Even 👽. Change only the ending state
+                // Even 👽. Change only the ending state
             } else if levelNum == 3{
-                Level(levelNum: $levelNum, level: "level\(levelNum)_", bodies: ["👽", "🌒"], answer: ["👽", "🌒", "👽", "🌒", "?", "?"], pickerXPos: [0.5, 0.93, 0.5, 0.06], pickerYPos: [0.05, 0.5, 0.96, 0.5], xPos: [0.7, 0.82, 0.5, 0.2, 0.2], yPos: [0.676, 0.15, 0.06, 0.16, 0.68], angles: [160, 50, 0, -50, -120])
+                Level(levelNum: $levelNum,
+                      level: "level\(levelNum)_",
+                      bodies: ["👽", "🌒"],
+                      answer: ["👽", "🌒", "👽", "🌒", "?", "?"],
+                      pickerXPos: [0.5, 0.93, 0.5, 0.06],
+                      pickerYPos: [0.05, 0.5, 0.96, 0.5],
+                      xPos: [0.7, 0.82, 0.5, 0.2, 0.2],
+                      yPos: [0.676, 0.15, 0.06, 0.16, 0.68],
+                      angles: [160, 50, 0, -50, -120])
             }
             // At least one occurence of 👽👽. That is, in your quest, you should capture at least once a consecutive 👽👽
+            else if levelNum == 4 {
+                Level(levelNum: $levelNum,
+                      level: "level\(levelNum)_",
+                      bodies: ["👽", "🌒"],
+                      answer: ["👽", "👽", "👽", "🌒", "🌒", "🌒"],
+                      pickerXPos: [0.5, 0.91, 0.88, 0.48, 0.1, 0.1],
+                      pickerYPos: [0.05, 0.37, 0.67, 0.96, 0.63, 0.32],
+                      xPos: [0.7, 0.82, 0.5, 0.2, 0.2],
+                      yPos: [0.76, 0.15, 0.06, 0.16, 0.68],
+                      angles: [160, 50, 0, -50, -120])
+            }
+            else {
+                VStack{
+                    Text("Ganhou, môpi!")
+                        .foregroundColor(.white)
+                        .bold()
+                        .font(.system(size: 50))
+                    // goodbye!
+                    
+                    Button("QUIT", action: {
+                        print("Out")
+                    })
+                    .buttonStyle(CustomButton(myColor: .white))
+                    .font(.system(size: 60))
+                }
+            }
+        }
+    }
+}
+
+struct OnBoardingView: View{
+    var body: some View{
+        VStack{
+            Text("Onboarding")
         }
     }
 }
@@ -58,6 +120,7 @@ struct Level : View {
     @State var selectedBodies: [String] = []
     //winning condition
     @State var won : Bool = false
+    @State var unlocked : Bool = true
     
     var body : some View{
         VStack{
@@ -66,11 +129,13 @@ struct Level : View {
                     GeometryReader {geo in
                         // Pickers
                         // Level 1 only has one picker
-                        MenuPicker(bodies: $bodies, selectedBody: $selectedBody1)
-                            .menuStyle(BodyMenu())
-                            .position(x: geo.size.width * pickerXPos[0], y: geo.size.height * pickerYPos[0])
+                        if unlocked{
+                            MenuPicker(bodies: $bodies, selectedBody: $selectedBody1)
+                                .menuStyle(BodyMenu())
+                                .position(x: geo.size.width * pickerXPos[0], y: geo.size.height * pickerYPos[0])
+                        }
                         // Levels 2 and 3 have four pickers
-                        if levelNum > 1 {
+                        if levelNum > 1 && unlocked {
                             MenuPicker(bodies: $bodies, selectedBody: $selectedBody2)
                                 .menuStyle(BodyMenu())
                                 .position(x: geo.size.width * pickerXPos[1], y: geo.size.height * pickerYPos[1])
@@ -80,9 +145,9 @@ struct Level : View {
                             MenuPicker(bodies: $bodies, selectedBody: $selectedBody4)
                                 .menuStyle(BodyMenu())
                                 .position(x: geo.size.width * pickerXPos[3], y: geo.size.height * pickerYPos[3])
-                            }
-                        // Levels 4 and 5 have 6 pickers
-                        if levelNum > 3 {
+                        }
+                        // Levels 4 and 5 have six pickers
+                        if levelNum > 3 && unlocked {
                             MenuPicker(bodies: $bodies, selectedBody: $selectedBody5)
                                 .menuStyle(BodyMenu())
                                 .position(x: geo.size.width * pickerXPos[4], y: geo.size.height * pickerYPos[4])
@@ -90,7 +155,7 @@ struct Level : View {
                                 .menuStyle(BodyMenu())
                                 .position(x: geo.size.width * pickerXPos[5], y: geo.size.height * pickerYPos[5])
                         }
-
+                        
                         // Rocket animation
                         Image("foguetin")
                             .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
@@ -107,27 +172,27 @@ struct Level : View {
                 )
             // Continue to next stage. It's only able to tap when winning condition is met
             Button("CONTINUE", action: {
+                unlocked.toggle()
                 levelNum+=1
             })
             .buttonStyle(CustomButton(myColor: .white)).padding()
             .disabled(!won)
             .onChange(of: [selectedBody1, selectedBody2, selectedBody3, selectedBody4, selectedBody5, selectedBody6]) { _ in
-                
                 selectedBodies = [selectedBody1, selectedBody2, selectedBody3, selectedBody4, selectedBody5, selectedBody6]
                 
-                print(selectedBodies)
+                //winning condition
                 if selectedBodies == answer {
                     timer.connect()
                     won.toggle()
                 }
             }
         }.onAppear {
-                self.selectedBodies.append(selectedBody1)
-                self.selectedBodies.append(selectedBody2)
-                self.selectedBodies.append(selectedBody3)
-                self.selectedBodies.append(selectedBody4)
-                self.selectedBodies.append(selectedBody5)
-                self.selectedBodies.append(selectedBody6)
+            self.selectedBodies.append(selectedBody1)
+            self.selectedBodies.append(selectedBody2)
+            self.selectedBodies.append(selectedBody3)
+            self.selectedBodies.append(selectedBody4)
+            self.selectedBodies.append(selectedBody5)
+            self.selectedBodies.append(selectedBody6)
         }
     }
 }
@@ -149,6 +214,7 @@ struct MenuPicker : View{
     }
 }
 
+// button customization
 struct CustomButton : ButtonStyle {
     var myColor : Color
     @Environment(\.isEnabled) var isEnabled
@@ -162,6 +228,7 @@ struct CustomButton : ButtonStyle {
     
 }
 
+// picker customization
 struct BodyMenu : MenuStyle{
     func makeBody(configuration: Configuration) -> some View {
         Menu(configuration)
@@ -178,11 +245,15 @@ struct Route : View {
     @State private var idx : Int = 0
     private let timer = Timer.publish(every: 0.7, on: .main, in: .common).autoconnect()
     var level : String
-    
     var body: some View{
+        // animation for space route arrows to move
         ZStack{
             Image(level + "1")
+                .resizable()
+                .scaledToFit()
             Image(level + "\(idx)")
+                .resizable()
+                .scaledToFit()
                 .animation(.easeInOut(duration: 2))
                 .onReceive(timer) { _ in
                     withAnimation{
