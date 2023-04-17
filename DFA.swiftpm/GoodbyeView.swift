63ea6fc1ac474d83ba72ebe9556cafc5286c25f4
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-04-17 16:43:46 -0300

    finished first version

--- DFA.swiftpm/GoodbyeView.swift
+++ DFA.swiftpm/GoodbyeView.swift
@@ -12,19 +12,32 @@ struct GoodbyeView: View{
             Color.black.ignoresSafeArea()
             Image("background").resizable().scaledToFill().ignoresSafeArea()
             VStack{
-                Text("You won!")
+                Text("YOU WON!")
                     .foregroundColor(.white)
                     .bold()
-                    .font(.largeTitle)
-                Text("This puzzle was created by André Valença for the WWDC Challenge...")
-                    .foregroundColor(.white)
+                    .font(.system(size: 60))
+                    .padding(.top, 60)
                 
+                Spacer()
+                Text("This puzzle was created\nby André Valença for the\nSwift Student Challenge of\nthe 2023 Apple Worldwide\nDevelopers Conference.\n\n Hope you liked it!")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+
+                Spacer()
                 Button("RETURN", action: {
                     self.isActive.toggle()
                 })
                 .buttonStyle(CustomButton(myColor: Color(red: 30/255, green: 0, blue: 148/255), myColor2: Color(red: 134/255, green: 0, blue: 0)))
+                .padding(.bottom, 60)
             }.navigationBarBackButtonHidden(true)
         }
     }
 }
 
+
+struct GoodbyeView_Previews: PreviewProvider {
+    static var previews: some View {
+        GoodbyeView(isActive: .constant(true))
+    }
+}
